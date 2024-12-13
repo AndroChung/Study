bbf801a253b4574a45e7fec06efbb3e9bf9aacf9
AndroChung <xoals5315@naver.com>
2024-12-13 17:58:16 +0900

    update

--- C, C++/C++/src/C++_practice/Ch3/3_1.cpp
+++ C, C++/C++/src/C++_practice/Ch3/3_1.cpp
@@ -1,49 +1,15 @@
 #include <iostream>
-
-class meterAndCentimeter {
-    private:
-    int meter;
-    int centimeter;
-    
-    public:
-    meterAndCentimeter(int meter, int centimeter) {
-        this->meter = meter;
-        this->centimeter = centimeter;
-    }
-
-    int getMeter() {
-        return meter;
-    }
-
-    int getCentimter() {
-        return centimeter;
-    }
-};
-
-class cmToMeterAndCentimeterChanger {
-    private:
-    const int CmPerMeter {100};
-
-    public:
-    meterAndCentimeter changeToMeterAndCentimeter(int centimeter) {
-        return meterAndCentimeter(centimeter / CmPerMeter, centimeter % CmPerMeter);
-    }
-};
-
 int main() {
     using namespace std;
-    
+
+    const int CmPerMeter {100};
+
     int height;
     cout << "Enter your height in centimeter: ___\b\b\b";
     cin >> height;
 
-    cmToMeterAndCentimeterChanger heightChanger;
-    meterAndCentimeter heightInMeterAndCentimeter = heightChanger.changeToMeterAndCentimeter(height);
-
-    int meter {heightInMeterAndCentimeter.getMeter()}, centimeter {heightInMeterAndCentimeter.getCentimter()};
-
     cout << "Your height " << height << "cm "
     << "is:\n";
-    cout << meter << "m " << centimeter << "cm.\n";
+    cout << height / CmPerMeter << "m " << height % CmPerMeter << "cm.\n";
     return 0;
 }
