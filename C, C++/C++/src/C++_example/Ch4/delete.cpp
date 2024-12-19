63c8ad63d2bb8b62a07d1bcc3fdf72df790c1384
AndroChung <xoals5315@naver.com>
2024-12-19 22:56:34 +0900

    Ch4 example upload

---
+++ C, C++/C++/src/C++_example/Ch4/delete.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+char * getname(void);
+int main() {
+    char * name;
+
+    name = getname();
+    cout << (int *) name << ": " << name << "\n";
+    delete [] name;
+
+    name = getname();
+    cout << (int *) name << ": " << name << "\n";
+    delete [] name;
+    return 0;
+}
+
+char * getname() {
+    char temp[80];
+    cout << "이름을 입력하십시오: " ;
+    cin >> temp;
+    char * pn = new char[strlen(temp) + 1];
+    strcpy(pn, temp);
+
+    return pn;
+}
